d9040308606b995f1090e2a0f27c8bd56da7be28
sarah-the-data-gal <159952161+sarah-the-data-gal@users.noreply.github.com>
2024-05-13 19:23:34 -0400

    Add files via upload

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,41 @@
+-- Creating Tables
+
+CREATE TABLE contacts(
+	contact_id int NOT NULL PRIMARY KEY, 
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(150) NOT NULL
+);
+CREATE TABLE category(
+	category_id VARCHAR(50) NOT NULL PRIMARY KEY,
+	category VARCHAR(50) NOT NULL
+);
+CREATE TABLE subcategory(
+	subcategory_id VARCHAR(50) NOT NULL PRIMARY KEY,
+	category VARCHAR(50) NOT NULL
+);
+CREATE TABLE campaign(
+	cf_id int NOT NULL,
+	contact_id int NOT NULL,
+	company_name VARCHAR(50) NOT NULL,
+	description VARCHAR(200) NOT NULL,
+	goal INT NOT NULL,
+	pledged INT NOT NULL,
+	outcome VARCHAR(50) NOT NULL,
+	backers_count INT NOT NULL,
+	country VARCHAR(2) NOT NULL,
+	currency VARCHAR(50) NOT NULL,
+	launched_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	category_id VARCHAR(50) NOT NULL,
+	subcategory VARCHAR(50) NOT NULL, 
+	PRIMARY KEY (cf_id),
+	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+	FOREIGN KEY (category_id) REFERENCES category(cateogry_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
+	
+	
+	
+	
+
